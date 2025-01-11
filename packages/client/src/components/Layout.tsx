4627b47317bced9e294ec40cceed094ba467c13b
yuwol <yuwol036@gmail.com>
2025-01-11 23:26:22 +0900

    feat: Add alias for src directory

--- packages/client/src/components/Layout.tsx
+++ packages/client/src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from './Header'
+import { Header } from '@/components/Header'
 import styles from './Layout.module.css'
 
 export function Layout() {
